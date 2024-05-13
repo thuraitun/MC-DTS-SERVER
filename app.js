d89370a06c90706b07162282648443050941f250
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2024-05-13 15:38:05 +0900

    rename folder

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 const app = express();
-import ApiError from "./src/utils/apiError.js";
-import router from "./src/routes/index.js";
+import ApiError from "./utils/apiError.js";
+import router from "./routes/index.js";
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
