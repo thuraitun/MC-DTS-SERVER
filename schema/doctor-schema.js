7c821348721bda3908c875850e2bd6d77bbd92f6
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2024-05-14 11:13:28 +0900

    add doctor schema

--- schema/doctor-schema.js
+++ schema/doctor-schema.js
@@ -10,6 +10,29 @@ const BASE_USER = z.object({
             specialist: z
                   .string()
                   .min(2, 'Specialist must have at least 2 characters.')
-                  .max(60, 'Specialist can only have 50 characters at most.')
+                  .max(60, 'Specialist can only have 60 characters at most.'),
+            experienc: z
+                  .number()
+                  .min(1, 'Experience must have at least 1 characters.')
+                  .max(3, 'Experience can only have 3 characters at most.'),
+            fee: z
+                  .number()
+                  .min(1, 'Fee must have at least 1 characters.')
+                  .max(7, 'Fee can only have 7 characters at most.'),
+            genter: z
+                  .string()
+                  .min(1, 'Genter must have at least 1 characters.')
+                  .max(6, 'Genter can only have 6 characters at most.')
+      }),
+});
+
+export const CREATE_DOCTOR = z.object({
+      body: BASE_USER.shape.body.strict()
+});
+
+export const UPDATE_DOCTOR = z.object({
+      body: BASE_USER.shape.body,
+      params: z.object({
+            id: z.string().refine(isObjectIdOrHexString),
       }),
 });
