72b580cbb676396eb7cbbdff6443dd9abd1f7092
thuraitun <thuraitun64@gmail.com>
2024-05-21 10:24:18 +0630

    added bio data default empty string in doctorSchema

--- schema/doctor-schema.js
+++ schema/doctor-schema.js
@@ -11,18 +11,21 @@ const BASE_USER = z.object({
 			.string()
 			.min(2, "Specialist must have at least 2 characters.")
 			.max(60, "Specialist can only have 60 characters at most."),
-		experience: z
+		experiences: z
 			.string()
 			.min(1, "Experiences must have at least 1 characters.")
 			.max(20, "Experiences can only have 20 characters at most."),
-		fee: z
-			.string()
-			.min(1, "Fee must have at least 1 characters.")
-			.max(8, "Fee can only have 7 characters at most."),
+		// fee: z
+		// 	.string()
+		// 	.min(1, "Fee must have at least 1 characters.")
+		// 	.max(8, "Fee can only have 7 characters at most."),
 		gender: z
 			.string()
 			.min(1, "Gender must have at least 1 characters.")
 			.max(6, "Gender can only have 6 characters at most."),
+		bio: z
+			.string()
+			.default("")
 	}),
 });
 
