91056c89122df4982b7137c6ead3bbf794fd6217
thuraitun <thuraitun64@gmail.com>
2024-05-29 09:48:00 +0630

    added vercel config

--- services/doctor-service.js
+++ services/doctor-service.js
@@ -23,7 +23,6 @@ export const getOneDoctorService = async (id) => {
 
 export const createDoctorService = async (body) => {
 	try {
-		console.log(body);
 		const { email } = body;
 		const isEmail = await Doctor.findOne({ email });
 		if (isEmail) throw ApiError.badRequest("This email is already in use");
@@ -31,7 +30,6 @@ export const createDoctorService = async (body) => {
 		const doctor = await Doctor.create(body);
 		return doctor;
 	} catch (error) {
-		console.log("Hello world!");
 		throw ApiError.notFound(error);
 	}
 };
